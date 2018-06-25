fb3bf28c5243d93fac9b06231302a4ffa0fec627
Krysta05 <martinezkrysta7@gmail.com>
2018-06-25 15:57:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,18 @@
+Class Song 
 
+Song.count
+  @@count = 0
+  
+  Song.artists
+ # => ["Jay-Z", "Drake", "Beyonce"]
+
+    Song.genres
+    @@genres = []
+  # => ["Rap", "Pop"]
+  
+  Song.genre_count
+  
+  
+  Song.artist_count
+  
+ attr_accesso  
